dd4719c224fdd8a4041889bf740b0867d876e867
Galang Abinerry <abinerrygalang@gmail.com>
2025-10-03 18:27:23 +0700

    Add files via upload

---
+++ mini-calculator.js
@@ -0,0 +1,37 @@
+a = 20;
+b = 15;
+
+const Perjumlahan = () => {
+    console.log(`Hasil dari 20 + 15 = ${a + b}`)
+}
+Perjumlahan();
+
+
+const Perkalian = () => {
+    console.log(`Hasil dari 20 x 15 = ${a * b}`)
+}
+Perkalian();
+
+
+const Pengurangan = () => {
+    console.log(`Hasil dari 20 - 15 = ${a - b}`)
+}
+Pengurangan();
+
+
+const Pembagian = () => {
+    console.log(`Hasil dari 20 / 15 = ${a / b}`)
+}
+Pembagian();
+
+
+const Modulus = () => {
+    console.log(`Hasil dari 20 % 15 = ${a % b}`)
+}
+Modulus()
+
+
+const Perpangkatan = () => {
+    console.log(`Hasil dari 20**15 = ${a ** b}`)
+}
+Perpangkatan()
